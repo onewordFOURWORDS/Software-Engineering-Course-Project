efa0486c8fd44cc9c109903e0e210560aa13a869
NickPerf33 <NickPerf33@gmail.com>
2022-09-27 15:46:50 -0400

    Update hello.py
    Added my name

--- hello.py
+++ hello.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Welcome to SSBM!</h1><h3>Created by Max, David, Scott, Tim ...</h3>"
+    return "<h1>Welcome to SSBM!</h1><h3>Created by Max, David, Scott, Tim, and Nick</h3>"
