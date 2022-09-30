7b5a0b8ae6b8d401bca54a0fd6d225a133a674ce
WilmingtonDavid <dmw1404@gmail.com>
2022-09-30 11:45:18 -0400

    Added classes

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from ctypes import addressof
+import email
+from tokenize import String
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +8,30 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<h1>Welcome to SSBM!</h1><h3>Created by Max, David, Scott, Tim, and Nick</h3>"
+
+class Team:
+    pass
+
+class User:
+    def __init__(self, email: str, hashedPassword: str, teamsToFollow: [Team], firstName: str, lastName:str,
+        address: str, phoneNumber: str):
+        self.email = email
+        self.hashedPassword = hashedPassword
+        self.teamsToFollow = teamsToFollow
+        self.firstName = firstName
+        self.lastName = lastName
+        self.address = address
+        self.phoneNumber = phoneNumber
+
+class Coach (User):
+    def __init__(self, teamsToFollow:[Team]):
+        self.teamsToFollow = teamsToFollow
+
+class Admin (User):
+    def __init__(self, teamsToFollow:[Team]):
+        self.teamsToFollow = teamsToFollow
+
+class Match:
+    def __init__(self, teamOne: Team, ):
+
+
