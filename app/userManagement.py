42cefc5a049dd4d32c269cb2c7db5061f7313e4e
onewordFOURWORDS <timdanham@gmail.com>
2022-10-04 17:52:59 -0400

    added userManagement api functions

---
+++ app/userManagement.py
@@ -0,0 +1,18 @@
+# These are the API functions for the User Management component
+
+def get_signed_in_user():
+    """
+    Return the user object that is currently signed in. Raises an exception if no user is signed in.
+    :param: none
+    :return: user object
+    """
+    return
+
+def is_user_signed_in():
+    """
+    Checks if a user is currently signed in to the website.
+    :param: none
+    :return: True | False
+    :rtype: boolean
+    """
+    return
