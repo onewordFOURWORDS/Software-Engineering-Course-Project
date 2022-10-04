b6e15a52cd26cc936f9d89a7a7c8792e718edb66
Max Roen <maxwell-roen@users.noreply.github.com>
2022-10-04 03:41:24 -0400

    trying to deploy to heroku pre updating anything

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'password'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'ssbm.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
