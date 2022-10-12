945ff9bf39c3743aa4d4e7230f597343450cce15
Max Roen <maxwell.roen@gmail.com>
2022-10-12 11:25:14 -0400

    postgres migration!changes to config.py, removes sqlite db in ssbm.db, new migrations

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from app import db, login
 from datetime import datetime
-from flask_login import UserMixin  # dont worry if pycharm gives a warning here
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 """
