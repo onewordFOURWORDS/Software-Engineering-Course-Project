44fc0e670f903594e1ca75f37589db3dd2ed2a9a
WilmingtonDavid <dmw1404@gmail.com>
2022-10-13 08:35:18 -0400

    Tournament creation validators

--- app/forms.py
+++ app/forms.py
@@ -12,9 +12,11 @@ from wtforms.validators import (
     DataRequired,
     Email,
     EqualTo,
+    Regexp
 )  # dont worry if pycharm gives a warning here
-from app.models import User
+from app.models import User, Tournament, League
 from flask_wtf.file import FileField
+from datetime import date
 
 
 class LoginForm(FlaskForm):
@@ -50,13 +52,28 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError("Please use a different email address.")
 
-
+"""
+I set up validators for all tournament creation fields. For tournament name, it must be unique, has to have something inside the field, and can only
+be letters, numbers, dashes and underscores. Tournament location has the same, except it doesnt have to be unique. Might set
+regex for tournament league in the future, but for now, it can be empty, but it still has the same regex as others. Tournament date
+must be set in the present or future, users should not be able to make a tournament in the past. 
+"""
 class TournamentCreationForm(FlaskForm):
-    tournamentName = StringField("Tournament Name", validators=[DataRequired()])
-    tournamentLocation = StringField("Tournament Location", validators=[DataRequired()])
-    tournamentLeague = StringField("Or Create A New League")
+    tournamentName = StringField("Tournament Name", validators=[DataRequired(), Regexp(regex=r'[ A-Za-z0-9_-]*$')])
+    tournamentLocation = StringField("Tournament Location", validators=[DataRequired(), Regexp(regex=r'[ A-Za-z0-9_-]*$')])
+    tournamentLeague = StringField("Or Create A New League", validators=[Regexp(regex=r'[ A-Za-z0-9_-]*$')])
     tournamentDate = DateField(
-        "TournamentDate", format="%Y-%m-%d", validators=[DataRequired()]
+        "TournamentDate", format="%Y-%m-%d", validators=[DataRequired()]    
     )
-    tournamentPicture = FileField("Tournament Picture")
+
+    def validate_tournamentDate(form, tournamentDate):
+        if tournamentDate.data < date.today():
+            raise ValidationError("Date must be set in the future.")
+
+    def validate_tournamentName(form, tournamentName):
+        tournamentString = tournamentName
+        if Tournament.query.filter_by(tournamentName=tournamentString.data).first():
+            raise ValidationError("Tournament name is already taken. Please choose a different name.")
+        
+
     submit = SubmitField("Create Tournament")
