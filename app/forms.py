e1e43bc203e3738935868ebf7fb357906777f4b4
onewordFOURWORDS <timdanham@gmail.com>
2022-10-14 17:39:20 -0400

    Basic email framework and password reset function

--- app/forms.py
+++ app/forms.py
@@ -57,3 +57,15 @@ class TournamentCreationForm(FlaskForm):
         "TournamentDate", format="%Y-%m-%d", validators=[DataRequired()]
     )
     submit = SubmitField("Create Tournament")
+
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
