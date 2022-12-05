5e9f3c61b0f451b6175a3a91e515a33b573c768b
Max Roen <maxwell-roen@users.noreply.github.com>
2022-12-05 01:23:37 -0500

    adds error message if you try to load prod config in dev environment.

--- app/__init__.py
+++ app/__init__.py
@@ -5,22 +5,32 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from jinja2 import Environment, FileSystemLoader
 from flask_mail import Mail
+import sqlalchemy
 
-app = Flask(__name__)
-app.config.from_object(ProdConfig)  # toggle me for dev vs prod.
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = "login"
-mail = Mail(app)
+try:
+    app = Flask(__name__)
+    app.config.from_object(ProdConfig)  # toggle me for dev vs prod.
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
+    login = LoginManager(app)
+    login.login_view = "login"
+    mail = Mail(app)
 
-from app.models import *
+    from app.models import *
 
-with app.app_context():
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
-env = Environment(loader=FileSystemLoader("./app/templates"))
-env.globals["is_admin"] = User.is_admin
-env.globals["is_coach"] = User.is_coach
+    env = Environment(loader=FileSystemLoader("./app/templates"))
+    env.globals["is_admin"] = User.is_admin
+    env.globals["is_coach"] = User.is_coach
 
-from app import routes
+    from app import routes
+
+except sqlalchemy.exc.OperationalError as e:
+    print(e)
+    print(
+        """You might be seeing this error if you are attempting to load the prod config object
+        in your local dev environement, see __init__.py line 12 and use DevConfig."""
+    )
+    exit(1)
--- config.py
+++ config.py
@@ -3,8 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-# Need to set ENV VARIABLES on Heroku, this approach should more or less still worK?
-# and this file can be in the repo then, that's fine?
 class DevConfig(object):
     SECRET_KEY = os.environ.get("SSBM_SECRET_KEY")
     PORT_NUMBER = os.environ.get("SSBM_DB_PORT_NUMBER")
@@ -34,7 +32,7 @@ class ProdConfig(object):
     host_string_start = "@"
     SECRET_KEY = db_url.split(username_terminator)[1].split(host_string_start)[0]
     # Can't edit this in Heroku Dashboard but apparently you must use
-    # postgres instead of postgresql as of sqlalchemy v1.40
+    # postgresql instead of postgres as of sqlalchemy v1.40
     # see https://stackoverflow.com/questions/66690321/flask-and-heroku-sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy
     # for details
     SQLALCHEMY_DATABASE_URI = db_url.replace("postgres://", "postgresql://", 1)
