0baf1be49c4141bde907dd771277b70bf217bf15
Max Roen <maxwell-roen@users.noreply.github.com>
2022-12-05 00:18:21 -0500

    config variable changes for deployment

--- config.py
+++ config.py
@@ -5,15 +5,20 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # TODO: Figure out best practice for managing the config file/making sure we keep keys etc out of the repo.
 # This is fine for now.
+# Need to set ENV VARIABLES on Heroku, this approach should more or less still worK?
+# and this file can be in the repo then, that's fine?
 class Config(object):
     SECRET_KEY = os.environ.get("SSBM_SECRET_KEY")
     # postgres 15 defaults to 5433, postgres 14 to 5432, I believe.
     PORT_NUMBER = os.environ.get("SSBM_DB_PORT_NUMBER")
-    DB_NAME = "app"
-    DB_USER = "postgres"
+    # DB_NAME = "app"
+    DB_NAME = os.environ.get("DB_NAME")
+    # DB_USER = "postgres"
+    DB_USER = os.environ.get("DB_USER")
 
     SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{DB_USER}:{SECRET_KEY}@localhost:{PORT_NUMBER}/{DB_NAME}"
+        # f"postgresql://{DB_USER}:{SECRET_KEY}@localhost:{PORT_NUMBER}/{DB_NAME}"
+        os.environ.get("DATABASE_URL")
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Email Configuration
